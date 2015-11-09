a25fded765fa133294aeb7714bb5e90fa8cb1751
Alexandra <alexandra.jaeger@uibk.ac.at>
2015-11-09 20:24:00 +0100

    initial commit for test project (so far: room, camera)

---
+++ projects/3DRoomTest/Assets/ToggleView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleView : MonoBehaviour {
+
+	public float thirdPersonDistance = -0.8f;
+
+	private bool firstPerson = false;
+
+
+	// Use this for initialization
+	void Start () {
+		firstPerson = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			if(firstPerson) {
+				transform.position += new Vector3(0,0,thirdPersonDistance);
+			} else {
+				transform.position -= new Vector3(0,0,thirdPersonDistance);
+			}
+			firstPerson = !firstPerson;
+		}
+	}
+}
